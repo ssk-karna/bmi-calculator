68048c478685a8f9b5ed338f281759f5ae407684
Satyam <satyamshashankkaran.ssk@gmail.com>
2022-07-19 17:05:44 +0530

    Created structure for homepage

---
+++ lib/input_page.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+
+const activeCardColor = Color( 0xFF1D1E33);
+const bottomContainerColour = Color( 0xFFEB1555);
+const bottomContainerHeight = 80.0;
+
+class InputPage extends StatefulWidget {
+  @override
+  _InputPageState createState() => _InputPageState();
+}
+
+class _InputPageState extends State<InputPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('BMI CALCULATOR'),
+      ),
+      body: Column(
+        children : <Widget>[
+          Expanded(
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: ReusableCard(colour :activeCardColor),
+    ),
+                Expanded(
+                  child: ReusableCard(colour :activeCardColor),
+              ),
+    ],
+    ),
+    ),
+
+    Expanded(
+    child: Row(
+    children: <Widget>[
+    Expanded(
+    child: ReusableCard(colour :activeCardColor),
+    ),
+    ],
+    ),
+    ),
+
+    Expanded(
+    child: Row(
+    children: <Widget>[
+      Expanded(
+    child: ReusableCard(colour :activeCardColor),
+    ),
+    Expanded(
+    child: ReusableCard(colour :activeCardColor),
+    ),
+    ],
+    ),
+    ),
+          Container(
+            color: bottomContainerColour,
+            margin: EdgeInsets.only(top:10),
+            width: double.infinity,
+            height: bottomContainerHeight,
+          )
+    ],
+    ),
+    );
+  }
+}
+
+class ReusableCard extends StatelessWidget {
+
+  ReusableCard({@required this.colour});
+  final Color? colour;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+          margin: EdgeInsets.all(15.0),
+      decoration : BoxDecoration(
+        color : colour,
+        borderRadius: BorderRadius.circular(10.0),
+        ),
+    );
+  }
+}
