344a6a279489ac06aefbbedddbba63ea421698d2
Satyam <satyamshashankkaran.ssk@gmail.com>
2022-07-30 05:11:57 +0530

    Created Result Page and Added Navigation

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -4,6 +4,8 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'reusable_card.dart';
 import 'icon_content.dart';
 import 'constants.dart';
+import 'results_page.dart';
+
 
 enum Gender { male, female }
 
@@ -179,11 +181,21 @@ class _InputPageState extends State<InputPage> {
               ],
             ),
           ),
-          Container(
-            color: kBottomContainerColour,
-            margin: EdgeInsets.only(top: 10),
-            width: double.infinity,
-            height: kBottomContainerHeight,
+          GestureDetector(
+            onTap: (){
+              Navigator.push(context, MaterialPageRoute(builder: (context) => ResultsPage()));
+            },
+            child: Container(
+              child: Center(
+                  child: Text('CALCULATE',style: kLargeButtonTextStyle,
+                  )
+              ),
+              color: kBottomContainerColour,
+              margin: EdgeInsets.only(top: 10),
+              padding: EdgeInsets.only(bottom: 10.0),
+              width: double.infinity,
+              height: kBottomContainerHeight,
+            ),
           )
         ],
       ),
---
+++ lib/results_page.dart
@@ -0,0 +1,43 @@
+import 'package:bmi_calculator/reusable_card.dart';
+import 'package:flutter/material.dart';
+import 'constants.dart';
+
+class ResultsPage extends StatelessWidget {
+  const ResultsPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('BMI CALCULATOR'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Expanded(child: Container(
+            child: Text('Your Result',style : kTitleTextStyle),
+          )
+          ),
+          Expanded(
+            flex: 5,
+            child: ReusableCard(
+              colour: kActiveCardColor,
+              cardChild: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Text('Normal', style: kResultTextStyle,),
+                  Text('18.3', style: kBMITextStyle,),
+                  Text('Your BMI result is quite low. You should eat more!',
+                    textAlign: TextAlign.center,
+                    style: kBodyTextStyle,),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
