acf572b7746245f429367cd19a996b0b96fb1afa
Satyam <satyamshashankkaran.ssk@gmail.com>
2022-07-30 18:09:34 +0530

    BottomButton Not working - fixed

--- lib/components/bottom_button.dart
+++ lib/components/bottom_button.dart
@@ -4,16 +4,13 @@ import 'package:bmi_calculator/constants.dart';
 class BottomButton extends StatelessWidget {
   BottomButton(this.onTap, this.buttonTitle);
 
-  final Function onTap;
+  final Function() onTap;
   final String buttonTitle;
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: () {
-        onTap;
-        //
-      },
+      onTap:onTap,
       child: Container(
         child: Center(
             child: Text(
@@ -25,7 +22,9 @@ class BottomButton extends StatelessWidget {
         padding: EdgeInsets.only(bottom: 10.0),
         width: double.infinity,
         height: kBottomContainerHeight,
+
       ),
+
     );
   }
 }
